a0e3d6effce586dc71d548afdaccff8dced02596
dongkun kang <dongkunkang@dong-geun-ui-MacBookPro.local>
2020-06-22 22:45:35 +0900

    평균계산

--- test.py
+++ test.py
@@ -6,6 +6,16 @@ class MyTest(TestCase):
     def test_constructor(self):
         fp = io.open("score.csv","r",encoding = "utf-8")
         raw_value = fp.readlines()
+        
+        sum = list()
+        avg = list()
+        self.asserIsNotNone(sum)
+
+        for i in range(len(raw_value)):
+            std = raw_value[i].split(",")
+            sum.append(int(std[2])+int(std[3])+int(std[4]))
+            avg.append(round(sum[i]/3,3))   #평균
+
 
 if(__name__=="__main__"):
     unittest.main()
